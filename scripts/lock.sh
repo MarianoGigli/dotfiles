a81f7b9cff4dfee3acc75b2f5d6708fe0e2a77a4
Mariano Gigli <marianogigli@gmail.com>
2017-10-17 22:39:19 -0300

    modify lock script and use i3lock instead of xautolock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
 icon="/home/mariano/Pictures/tux-icon.png"
 tmpbg='/tmp/screen.png'
 
-(( $# )) && { icon=$1; }
+# (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
