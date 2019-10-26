9631bab77508e2c1b4c4b7e7906998b4013a54a3
Mariano Gigli <marianogigli@gmail.com>
2019-10-26 00:14:42 -0300

    tweak i3 and keyboard

--- bin/.local/bin/lock.sh
+++ bin/.local/bin/lock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
-icon="/home/mariano/Pictures/tux-icon.png"
+icon="/home/mariano/Pictures/tux-lock.png"
 tmpbg='/tmp/screen.png'
 
 # (( $# )) && { icon=$1; }
 
-scrot "$tmpbg"
+scrot -o "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" -blur 0x9 "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
+i3lock -i "$tmpbg"
